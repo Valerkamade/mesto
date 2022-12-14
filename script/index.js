6be4cf49efc7c03b33ee60114d1ec3b87b227f5f
Valeriya Izotova <my@valerka-made.ru>
2022-12-15 01:01:44 +0300

    style: добавить пробелы, отступы, комментарии

--- script/index.js
+++ script/index.js
@@ -1,31 +1,34 @@
-const popup = document.querySelector('.popup'); //
-const btnEdit = document.querySelector('.profile__btn-edit'); //
-const popupClose = popup.querySelector('.popup__btn-close');
-let nameInput = popup.querySelector('.popup__name');
-let jobInput = popup.querySelector('.popup__job');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__job');
+const popup = document.querySelector('.popup'); //Поиск попапа
+const btnEdit = document.querySelector('.profile__btn-edit'); //Поиск кнопки редактирования профиля
+const popupClose = popup.querySelector('.popup__btn-close'); //Поиск кнопки закрытия попапа
+let nameInput = popup.querySelector('.popup__name'); //Поиск поля формы имя попапа
+let jobInput = popup.querySelector('.popup__job'); //Поиск поля формы работа попапа
+let profileName = document.querySelector('.profile__name'); //Поиск данных имени
+let profileJob = document.querySelector('.profile__job'); //Поиск данных работы
 
-  const formOpen = function() {
+//Функция отрытия попапа, подтягивает имя и работу с полей профиля в форму
+const formOpen = function () {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
   popup.classList.add('popup_opened');
-}
+};
 
-btnEdit.addEventListener('click', formOpen);
-
-  const formClose = function() {
+//Функция закрытия попапа, подтягивает имя и работу с полей профиля в форму без сохранения изменений
+const formClose = function () {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
   popup.classList.remove('popup_opened');
-}
+};
 
-  const formSubmitHandler = function(evt) {
+//Функция сохранения изменений в полях формы и в профиле с закрытием окна
+const formSubmitHandler = function (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   formClose();
-}
+};
 
+//Слушатели событий
+btnEdit.addEventListener('click', formOpen);
 popupClose.addEventListener('click', formClose);
 popup.addEventListener('submit', formSubmitHandler);
