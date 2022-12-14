8be9cfd3c2ac0cba73a0a40992b05d54de0fc4dc
Valeriya Izotova <my@valerka-made.ru>
2022-12-14 23:34:25 +0300

    fix: иправить закрытие в скрипте

--- script/index.js
+++ script/index.js
@@ -6,7 +6,7 @@ let jobInput = popup.querySelector('.popup__job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
-function formOpen() {
+  const formOpen = function() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
   popup.classList.add('popup_opened');
@@ -14,12 +14,13 @@ function formOpen() {
 
 btnEdit.addEventListener('click', formOpen);
 
-function formClose() {
-  evt.preventDefault();
+  const formClose = function() {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
   popup.classList.remove('popup_opened');
 }
 
-function formSubmitHandler(evt) {
+  const formSubmitHandler = function(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
