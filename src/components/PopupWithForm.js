ee0815a333fdf60507c353e1242b3da6ffa9b8b7
Valeriya Izotova <my@valerka-made.ru>
2023-03-07 12:32:18 +0300

    feat: добавить класс профиля и увязать все классы между собой

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,21 +1,35 @@
 import Popup from "./Popup";
 
 export default class PopupWithForm extends Popup {
-  constructor(popupSelector, submitCallback) {
+  constructor(popupSelector, submitCallback, fillInputs) {
     super(popupSelector);
-    this.submitCallback = submitCallback;
-    this._formSubmit = this._popupElement.querySelector('.popup__form');
+    this._submitCallback = submitCallback;
+    this._fillInputs = fillInputs;
+    this._formSubmit = this._popupElement.querySelector('form');
+    this._inputList = Array.from(this._formSubmit.querySelectorAll('input'));
+    this._inputs = {};
   }
 
+  // Метод сбора данных инпутов формы
   _getInputValues() {
-
+    this._inputList.forEach((inputElement) => {
+      const inputName = inputElement.getAttribute('name')
+      this._inputs[inputName] = inputElement.value;
+    });
+    return this._inputs;
   }
 
+  // Метод установки слушателей 
   setEventListeners() {
     super.setEventListeners();
-
-    this._formSubmit.addEventListener('submit', this.submitCallback);
+    this._formSubmit.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this._submitCallback(this._getInputValues());
+      this.close();
+    });
   }
+
+  // Метод закрытия попапа со сбросом формы
   close() {
     super.close();
     this._formSubmit.reset();
